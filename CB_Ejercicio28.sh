4cb45bdf1b9677e3aaa7c1c3e13dda3a7732560d
Cristian Borja <cm.borja10@uniandes.edu.co>
2018-11-15 23:44:29 -0500

    Add files via upload

--- CB_Ejercicio28.sh
+++ CB_Ejercicio28.sh
@@ -2,9 +2,9 @@
 #PBS -l nodes=1:ppn=1, mem=1gb,walltime=00:05:00
 #PBS -M cm.borja10@uniandes.edu.co
 #PBS -m abe
-#PBS -N CristianBorja_Ejercicio27
+#PBS -N CristianBorja_Ejercicio28
 
-cd /hpcfs/home/fisi4028/cm.borja10/CristianBorja_Ejercicio27/
+cd /hpcfs/home/fisi4028/cm.borja10/CristianBorja_Ejercicio28/
 
 module load mpich/3.2.1
 
